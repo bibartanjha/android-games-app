0d2eeacfe0074996d2e288ac7ebeb4d3087da094
Bibartan Jha <jha.b1bartan@utexas.edu>
2025-01-05 04:39:23 -0600

    updated GameRow field names

--- app/src/main/java/com/example/android_games_app/games/frogger/utils/GameRow.kt
+++ app/src/main/java/com/example/android_games_app/games/frogger/utils/GameRow.kt
@@ -5,7 +5,7 @@ import com.example.android_games_app.games.frogger.utils.RowObject.RowObjectType
 data class GameRow(
     val rowType: GameRowType,
     val objectsAreGoingLeft: Boolean = false,
-    val objectTypeInLane: RowObjectType = RowObjectType.NONE,
-    val containsWiderObject: Boolean = false,
+    val objectsInLane: List<RowObjectType> = emptyList(),
+    val numColumnsTakenUpByEachObject: Float = 1f,
     var yOffsetValueForRow: Float = 0f
 )
